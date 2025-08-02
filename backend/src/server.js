2ac48734dfc1672ae47ec4a10150d9aff80c4f3d
nishmithashettigar <nishmithashettigar022@gmail.com>
2025-08-02 09:53:57 +0530

    initial commit

---
+++ backend/src/server.js
@@ -0,0 +1,42 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+import notesRoutes from "./routes/notesRoutes.js";
+import { connectDB } from "./config/db.js";
+import rateLimiter from "./middleware/rateLimiter.js";
+
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5001
+
+
+
+//middleware
+app.use(cors({
+    origin:"http://localhost:5173",
+}));
+
+app.use(express.json()); // this middlewae will parse JSON bodies: req.body
+app.use(rateLimiter);
+
+// our simple custom middleware
+// app.use((req, res, next) =>{
+//     console.log(`Req methos is ${req.method} & Req URL is ${req.url}`);
+//     next();
+// });
+
+app.use("/api/notes", notesRoutes);
+
+connectDB().then(() => {
+
+app.listen(5001, () =>{
+    console.log("Server started on PORT:",PORT);
+});
+})
+
+
+
+
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import {Link} from "react-router";
+import { PlusIcon } from "lucide-react";
+const Navbar = () => {
+  return (<header className="bg-base-300 border-b border-base-content/10 sticky top-0 z-50 pt-2">
+    <div className="mx-auto max-w-6xl px-4">
+        <div className="flex items-center justify-between">
+            <h1 className="text-3xl font-bold text-primary font-mono tracking-tight">ThinkBoard</h1>
+            <div className="flex items-center gap-4">
+                <Link to={"/create"} className="btn btn-primary">
+                    <PlusIcon className="size-5"/>
+                    <span>New Note</span>
+                </Link>
+            </div>
+        </div>
+    </div>
+  </header>);
+};
+
+export default Navbar;
